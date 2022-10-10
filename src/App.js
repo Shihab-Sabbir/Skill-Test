19d0041944392a0c5d70db29d02d498b4df73447
shihab-sabbir <shihab11231@gmail.com>
2022-10-10 21:06:52 +0600

    route created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './Layout/Layout';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/', element: <Layout />, children: [
+
+      ]
+    },
+  ]);
   return (
-    <div>App</div>
+    <RouterProvider router={router}></RouterProvider>
   )
 }
 
-export default App
+export default App;
