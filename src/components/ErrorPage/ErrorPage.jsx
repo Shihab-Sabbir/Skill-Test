f24ba0575299128fcd5fa5a001cc0b9d8a0bbaeb
shihab-sabbir <shihab11231@gmail.com>
2022-10-12 00:56:15 +0600

    read me file update

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -3,17 +3,21 @@ import { Link, useRouteError } from 'react-router-dom';
 
 function ErrorPage() {
     const error = useRouteError();
+    console.log('error : ', error);
     return (
         <div className='text-white'>
             <section className="flex items-center h-full p-16 dark:bg-gray-900 dark:text-gray-100">
                 <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
                     <div className="max-w-md text-center">
                         <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
-                            <span className="sr-only">Error</span>{error.status}
+                            <span className="sr-only">Error</span>{error?.status}
                         </h2>
-                        <p className="text-2xl font-semibold md:text-3xl">{error.statusText}</p>
-                        <p className="mt-4 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>
-                        <Link rel="noopener noreferrer" to="/" className="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Back to homepage</Link>
+                        <p className="text-2xl font-semibold md:text-3xl">{error?.statusText}</p>
+                        {error && <p className="mt-4 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>}
+                        {error === undefined && <p className="text-justify mt-4 mb-8 dark:text-gray-400">Opps ! Sothing is wrong , please check your URL is correct or not but dont worry, you can find plenty of other things on our homepage.</p>}
+                        <Link to='/'>
+                            <button className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'>Back to homepage</button>
+                        </Link>
                     </div>
                 </div>
             </section>
--- src/components/QuizScript/QuizScript.jsx
+++ src/components/QuizScript/QuizScript.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useLoaderData } from 'react-router-dom'
+import ErrorPage from '../ErrorPage/ErrorPage';
 import Options from './Options';
 
 function QuizScript() {
@@ -7,15 +8,20 @@ function QuizScript() {
     const [correctAns, setCorrectAns] = useState(0)
     const script = useLoaderData();
     const { id, name, questions } = script.data;
+
     return (
         <div className='py-10'>
-            <p className='text-center font-bold text-2xl text-white'>{name} Quiz</p>
-            <div className='text-center  flex flex-col md:flex-row justify-evenly p-2 border m-4 lg:max-w-[970px] lg:mx-auto lg:p-4'>
-                <p className='text-white'>Total Question  : {questions.length}</p>
-                <p className='text-white'>Answered Question : {answered.length}</p>
-                <p className='text-white'>Correct Answer : {correctAns} </p>
-                <p className='text-white'>Wrong Answer : {answered.length - correctAns} </p>
-            </div>
+            {questions?.length === undefined ? <ErrorPage /> :
+                <div>
+                    <p className='text-center font-bold text-2xl text-white'>{name} Quiz</p>
+                    <div className='text-center  flex flex-col md:flex-row justify-evenly p-2 border m-4 lg:max-w-[970px] lg:mx-auto lg:p-4'>
+                        <p className='text-white'>Total Question  : {questions?.length || 0}</p>
+                        <p className='text-white'>Answered Question : {answered?.length}</p>
+                        <p className='text-white'>Correct Answer : {correctAns} </p>
+                        <p className='text-white'>Wrong Answer : {answered?.length - correctAns} </p>
+                    </div>
+                </div>
+            }
             <div className='grid grid-cols-1 gap-4 p-1 sm:p-2 md:p-4 lg:max-w-[1000px] mx-auto'>
                 {questions?.map((question, idx) => <Options key={question.id} answered={answered} setAnswered={setAnswered} index={idx} singleQuestion={question} setCorrectAns={setCorrectAns}></Options>)}
             </div>
