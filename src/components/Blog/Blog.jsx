65626efc019fc9377381984f76e58d5b8887be1e
shihab-sabbir <shihab11231@gmail.com>
2022-10-11 22:03:16 +0600

    landing page updated

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -7,11 +7,13 @@ function Blog() {
             <Accordion>
                 <Accordion.Panel>
                     <Accordion.Title>
-                        What is Flowbite?
+                        Why use React Router?
                     </Accordion.Title>
                     <Accordion.Content>
-                        <p className="mb-2 text-white">
-                            Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.
+                        <p className="mb-2 text-white text-justify">
+                            React Router, and dynamic, client-side routing, allows us to build a single-page web application with navigation without the page refreshing as the user navigates. React Router uses component structure to call components, which display the appropriate information.
+                            <br />
+                            By preventing a page refresh, and using Router or Link. This is one increasingly common way of having a more seamless user experience. React router also allows the user to utilize browser functionality like the back button and the refresh page while maintaining the correct view of the application.
                         </p>
                     </Accordion.Content>
                 </Accordion.Panel>
@@ -20,10 +22,10 @@ function Blog() {
                         What is Context API and How it works?
                     </Accordion.Title>
                     <Accordion.Content>
-                        <p className="mb-2 text-white">
+                        <p className="mb-2 text-white text-justify">
                             The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent.
                         </p>
-                        <p className="mb-2 text-white">
+                        <p className="mb-2 text-white text-justify">
                             React.createContext() is the first this to create . It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state.
                         </p>
                     </Accordion.Content>
@@ -33,10 +35,10 @@ function Blog() {
                         What is React useRef and How to Use It?
                     </Accordion.Title>
                     <Accordion.Content>
-                        <p className="mb-2 text-white">
+                        <p className="mb-2 text-white text-justify">
                             useRef is a hook introduced with other hooks in React version 16.8 and is mainly used for creating a reference of the DOM element or directly accessing it inside a functional component.
                         </p>
-                        <p className="mb-2 text-white">
+                        <p className="mb-2 text-white text-justify">
                             Essentially, useRef is a hook function that gets assigned to a variable, inputRef, and then attached to an attribute called ref inside the HTML element you want to reference.
                             Common cases this hook is used to manipulate DOM.In plain JavaScript you had to use getElementById or querySelector to select a DOM node, in React useRef serves the DOM manipulation features.
                         </p>
