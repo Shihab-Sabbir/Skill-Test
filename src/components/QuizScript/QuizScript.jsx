b9eed5be5f2d4925301f6b809f36cc3ad914ae22
shihab-sabbir <shihab11231@gmail.com>
2022-10-12 13:27:40 +0600

    few update on context api

--- src/components/QuizScript/QuizScript.jsx
+++ src/components/QuizScript/QuizScript.jsx
@@ -15,7 +15,7 @@ function QuizScript() {
                 <div>
                     <p className='text-center font-bold text-2xl text-white'>{name} Quiz</p>
                     <p className='text-center text-yellow-400 p-2'><small>**Only first answer will be counted correct or wrong for each question**</small></p>
-                    <div className='text-center  flex flex-col md:flex-row justify-evenly p-2 border m-4 lg:max-w-[970px] lg:mx-auto lg:p-4'>
+                    <div className='text-center  flex flex-col md:flex-row justify-evenly p-2 border m-4 lg:max-w-[970px] lg:mx-auto lg:p-4 font-mono'>
                         <p className='text-white'>Total Question  : {questions?.length || 0}</p>
                         <p className='text-white'>Answered Question : {answered?.length}</p>
                         <p className='text-white'>Correct Answer : {correctAns} </p>
